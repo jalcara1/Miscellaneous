282dbfced55a9b4e697d58cf5f6907cb9f3d4a42
Juan Pablo Alcaraz Florez <jalcara1@eafit.edu.co>
2017-12-18 17:38:07 -0500

    Solve equation but some problems with tolerance

--- PageRank.cpp
+++ PageRank.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib> //srand, rand
+#include <cmath>
 using namespace std;
 
 typedef long double ld;
@@ -9,29 +10,59 @@ const int MAXN = 10;
 int main(void){
   ld** stochasticM; // Stochastic Matrix With The Pages Links
   stochasticM = new ld* [MAXN];
-  ll Fu =0; //Forward-Links
-  ll pages =0; //Number Of Forward-Links By One Page
+  ld* X;
+  ld* PageRank;
+  X = new ld[MAXN];
+  PageRank = new ld[MAXN];
+  /* Forward-Links - Number Of Forward-Links By One Page */
+  ll Fu, pages =0, i, j;
+  ld tolerance = 0.1, result =0, val, SUMA =0;
   srand(time(NULL));
-  for(ll i =0; i< MAXN; ++i){
+  for(i =0; i< MAXN; ++i){
     stochasticM[i] = new ld[MAXN];
-    for(ll j =0; j< MAXN; ++j){
+    X[i] = 1/(ld)MAXN;
+    for(j =0; j< MAXN; ++j){
       stochasticM[i][j] = 0;
     }
   }  
-  for(ll i =0; i< MAXN; ++i){
+  for(i =0; i< MAXN; ++i){
     pages = 1 + rand()%MAXN;
-    for(ll j =0; j< pages; ++j){          
+    for(j =0; j< pages; ++j){          
       do{
 	Fu = 0 + rand()%(MAXN);
       }while(Fu == i);
       stochasticM[Fu][i] += 1/(ld)pages;
     }
   }
-  for(ll i =0; i< MAXN; ++i){
-    for(ll j =0; j< MAXN; ++j){
-      cout << stochasticM[i][j] << " ";
+  do{
+    for(i =0; i< MAXN; ++i){
+      for(j =0; j< MAXN; ++j){
+	PageRank[i] += stochasticM[i][j]*X[j];
+      }
     }
-    cout << endl;
+    for(i =0; i< MAXN; ++i){
+      val = abs(PageRank[i]-X[i]);
+      if(val > result){
+	result = val;
+      }
+    }
+    cout << "->> " << result << endl;
+    if(tolerance > result){
+      cout << "AQUI" << endl;
+      for(i =0; i< MAXN; ++i){
+	X[i] = PageRank[i];
+      }
+    }
+  }while(tolerance < result);
+  
+  for(i =0; i< MAXN; ++i){
+    cout << PageRank[i] << endl;
+    SUMA += PageRank[i];
+    // for(j =0; j< MAXN; ++j){
+    //   cout << stochasticM[i][j] << " ";
+    // }
+    // cout << endl;
   }
+  cout << "--»» " << SUMA << endl;
   return 0;
 }
