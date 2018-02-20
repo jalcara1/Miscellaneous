368f1ae21f84ceb9652a183c7ab020e3b3decae3
Juan Pablo Alcaraz Florez <jalcara1@eafit.edu.co>
2018-02-20 09:35:35 -0500

    MPI Example with Send - Recv

--- HelloProcess.cpp
+++ HelloProcess.cpp
@@ -3,15 +3,13 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-  int rank;
-  size_t hostSize = 19;
-  char host[hostSize];
-
+  int rankId, tasks;
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  gethostname(host, hostSize);
-  printf("The process number %d is running on %s \n", rank, host);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rankId);
+  MPI_Comm_size(MPI_COMM_WORLD, &tasks);
+  
+  //gethostname(host, hostSize);
+  printf("The process number %d of %d, ¡Says Hello! \n", rankId, tasks);
 
   MPI_Finalize();
   return 0;
--- SendRecv.cpp
+++ SendRecv.cpp
@@ -1,22 +1,26 @@
 #include "stdio.h"
 #include "mpi.h"
 
+MPI_Status status;
+
 int main(int argc, char** argv){
 
-  int rank, dataSize =3, src =0, dest =1, tag =0;
-  int data[] = {1,2,3},
+  int rank, tasks, dataSize =3, src =0, tag =0;
+  int data[] = {1,2,3};
     
-  MPI_Status status;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  if(rank == 0)
-    MPI_Send(data, dataSize, MPI_INT, dest, tag, MPI_COMM_WORLD);
-  if(rank == 1){
+  MPI_Comm_size(MPI_COMM_WORLD, &tasks);
+  
+  if(rank == 0){
+    for(int node =1; node<= tasks; ++node){
+      MPI_Send(data, dataSize, MPI_INT, node, tag, MPI_COMM_WORLD);
+    }
+  }else{
     MPI_Recv(data, dataSize, MPI_INT, src, tag, MPI_COMM_WORLD, &status);
-    printf("Data[0] = %d\n", data[0]);
-    printf("Data[1] = %d\n", data[1]);
-    printf("Data[2] = %d\n", data[2]);
+    printf("Process %d Data[0] = %d\n", rank, data[0]);
+    printf("Process %d Data[1] = %d\n", rank, data[1]);
+    printf("Process %d Data[2] = %d\n", rank, data[2]);
   }
   MPI_Finalize();
   return 0;
